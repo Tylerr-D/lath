27c9b04b8e8e35af7bb8913f70013c306094bdfc
Tylerr-D <tylermishra76@gmail.com>
2026-09-01 00:07:50 +0530

    incomp funcs

---
+++ src/compiler/bytecode.rs
@@ -0,0 +1,19 @@
+use::crate::ast::{Node, Operator};
+
+#[derive(Debug, PartialEq)]
+pub enum OpCode {
+    Constant(i32),
+    Add,
+    Subtract,
+    Negate.
+}
+
+pub fn compile(ast: &[Node]) -> Vec<OpCode> {
+    let mut code = Vec::new();
+
+    for node in ast {
+        compile_node(node, &mut code);
+        
+    }
+
+}
