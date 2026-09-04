065e584801e493ff1df40e5e7f98a3e11e0e6bfd
Tylerr-D <tylermishra76@gmail.com>
2026-09-04 23:01:51 +0530

    coverts ast nodes to bytecode

--- src/compiler/vm/opcode.rs
+++ src/compiler/vm/opcode.rs
@@ -1,14 +1,49 @@
 pub enum OpCode {
     OpConstant(u16),
-    OpPOp,
+    OpPop,
     OpAdd,
     OpSub,
     OpPlus,
     OpMinus,
 }
 
+fn to_bytes(op: OpCode) -> Vec<u8> {
+
+match op {
 OpCode::OpConstant(arg) => make_three_bytes_op(0x01, arg),
 OpCode::OpPop => vec![0x02],
 OpCode::OpAdd => vec![0x03],
 OpCode::OpSub => vec![0x04],
 OpCode::OpPlus => vec![0x0A],
+OpCode::OpMinus => vec![0x0B],
+}
+}
+
+pub struct Bytecode {
+    pub instructions: Vec<u8>,
+    pub constants: Vec<Node>,
+}
+
+pub struct Interpreter {
+    bytecode: Bytecode,
+}
+
+impl Compile for Interpreter {
+    type Output = Bytecode; 
+
+
+fn from_ast(ast: Vec<Node>) -> Self::Output {
+            let mut interpreter = Interpreter {
+            bytecode: Bytecode::new(),
+};
+
+for node in ast {
+    println!("compliling node {:?}",node);
+    interpreter.interpret_node(node);
+     interpreter.add_instruction(OpCode::OpPop);
+}
+
+        interpreter.bytecode
+
+}
+}
