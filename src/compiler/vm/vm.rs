7fdff2332a04e5ded693bc21157c15645a782abe
Tylerr-D <tylermishra76@gmail.com>
2026-09-04 23:17:38 +0530

    wrote vm configs

---
+++ src/compiler/vm/vm.rs
@@ -0,0 +1,10 @@
+// these are my personal notes pls dont mind them and umm not ai 
+// ok so here we gonna first take bytecode
+// bytecodes are instructions
+// then we got our big ahh VM
+// VM reads the byte and understands what it means with the help of our opcode.rs that i js wrote
+// now the vm gonna through everything to the stack
+// does the operation required
+// moves to the next byte does the same shit again
+// lets fucking go
+
